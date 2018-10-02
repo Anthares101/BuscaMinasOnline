a7ce219b09e715a1f25dc1a31b83d6232c4e3c58
Angel Heredia <angel.angel802@gmail.com>
2018-10-02 17:42:43 +0200

    main.cpp added for testing

--- buscaMinas.hpp
+++ buscaMinas.hpp
@@ -1,13 +1,26 @@
 #ifndef _BUSCAMINAS_HPP_
 #define _BUSCAMINAS_HPP_
 
+#include<cstdlib>
+#include<ctime>
 #include <vector>
 
 class buscaMinas{
 	private:
-		std::vector< vector <int> > v; > v;
+		//Attributes
+		std::vector< std::vector<int> > board; //Empty game board
+
+		//Private Methods
+
 	public:
-		
+		//Builder
+		inline buscaMinas(){
+			board = std::vector< std::vector<int> >(10, std::vector< int >(10)); //10x10 game Board
+			srand(time(NULL)); //Random number seed initialized
+		};	
+
+		//Public Methods
+
 };
 
 #endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "buscaMinas.hpp"
+
+int main(){
+
+std::cout << "Prueba buscaminas 1.0" << std::endl;
+
+buscaMinas board1;
+
+return 0;
+}
